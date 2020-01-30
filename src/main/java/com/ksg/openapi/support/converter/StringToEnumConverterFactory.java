2a3c6dbf160a6935fb0691f5e127c2ce5e8be407
sgkang <sgkang14@gmail.com>
2020-01-31 01:22:03 +0900

    1/31 01:21

--- src/main/java/com/ksg/openapi/support/converter/StringToEnumConverterFactory.java
+++ src/main/java/com/ksg/openapi/support/converter/StringToEnumConverterFactory.java
@@ -1,14 +1,15 @@
 package com.ksg.openapi.support.converter;
 
+import com.ksg.openapi.common.code.EnumCode;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
 
 /**
  * Created by 강성근
  */
-public class StringToEnumConverterFactory implements ConverterFactory<String, Enum> {
+public class StringToEnumConverterFactory implements ConverterFactory<String, EnumCode> {
 
-    private static class StringToEnumConverter<T extends Enum>
+    private static class StringToEnumConverter<T extends EnumCode>
             implements Converter<String, T> {
 
         private Class<T> enumType;
@@ -18,12 +19,21 @@ public class StringToEnumConverterFactory implements ConverterFactory<String, En
         }
 
         public T convert(String source) {
-            return (T) Enum.valueOf(this.enumType, source.trim().toUpperCase());
+
+            final T[] enumConstants = enumType.getEnumConstants();
+
+            for (final T enumConstant : enumConstants) {
+                if (enumConstant.getValue().equals( source )) {
+                    return enumConstant;
+                }
+            }
+            return null;
+//            return (T) Enum.valueOf(this.enumType, source.trim().toUpperCase());
         }
     }
 
     @Override
-    public <T extends Enum> Converter<String, T> getConverter(
+    public <T extends EnumCode> Converter<String, T> getConverter(
             Class<T> targetType) {
         return new StringToEnumConverter(targetType);
     }
